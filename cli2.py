ec20796ad8694c7272668d93d3c0f76f7f7abe29
chedvi10 <c0556731959@gmail.com>
2024-07-30 14:05:16 +0300

    changes

--- cli2.py
+++ cli2.py
@@ -3,15 +3,15 @@ from pathlib import Path
 
 
 parser = argparse.ArgumentParser('replace old str to new str')
-parser.add_argument('path', type = str, help='path to replace str')
-parser.add_argument('old', type = str, help='str to change')
-parser.add_argument('new',type = str, help ='new str')
+parser.add_argument('path', type=str, help='path to replace str')
+parser.add_argument('old', type=str, help='str to change')
+parser.add_argument('new',type=str, help='new str')
 
 args = parser.parse_args()
 file_to_change = Path(args.path)
-for file_path  in file_to_change.rglob('*'):
+for file_path in file_to_change.rglob('*'):
     if file_path .is_file():
-        with open(file_path , 'r') as file:
+        with open(file_path, 'r') as file:
             lines = file.readlines()
 
         with open(file_path, 'w') as file_write:
--- cli3.py
+++ cli3.py
@@ -1,5 +1,3 @@
-import os
-import sys
 import argparse
 import pathlib
 
@@ -7,5 +5,5 @@ parser = argparse.ArgumentParser()
 parser.add_argument('path', type=str)
 parser.add_argument('new_name', type=str)
 args = parser.parse_args()
-nameFile=pathlib.Path(args.path)
+nameFile = pathlib.Path(args.path)
 nameFile.rename(nameFile.with_stem(args.new_name))
