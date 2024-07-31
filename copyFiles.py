1c8b8ea935a48689a55bedde47ce1852f7344b02
chedvi10 <c0556731959@gmail.com>
2024-07-31 09:45:29 +0300

    changes

--- copyFiles.py
+++ copyFiles.py
@@ -1,11 +1,10 @@
 import argparse
 from pathlib import Path
 
-
 parser = argparse.ArgumentParser('replace old str to new str')
 parser.add_argument('path', type=str, help='path to replace str')
 parser.add_argument('old', type=str, help='str to change')
-parser.add_argument('new',type=str, help='new str')
+parser.add_argument('new', type=str, help='new str')
 
 args = parser.parse_args()
 file_to_change = Path(args.path)
@@ -18,5 +17,3 @@ for file_path in file_to_change.rglob('*'):
             for line in lines:
                 new_line = line.replace(args.old, args.new)
                 file_write.write(new_line)
-
-
